c28001e32dee536af32587418b0149e35f325302
Yogev Levy <MrYogevLevy@gmail.com>
2015-12-17 11:23:30 +0200

    connecting to the local DB

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://Admin2:Yusuf1234@ec2-52-24-117-86.us-west-2.compute.amazonaws.com:27017/dummyDB'
 }

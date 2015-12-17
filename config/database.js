aff0cb26376de1324e59d8fa215dad3ab752d2e5
root <mryogevlevy@gmail.com>
2015-12-17 10:44:27 +0000

    Oh god, please work

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://Admin2:Yusuf1234@ec2-52-24-117-86.us-west-2.compute.amazonaws.com:27017/dummyDB'
+	url : 'mongodb://Yusuf:Yusuf1234@ec2-52-24-117-86.us-west-2.compute.amazonaws.com:27017/nodeAppDB'
 }
